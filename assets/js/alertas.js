99e4d4e357b5d104e76917d6cc71d78c0ac14b69
juanblas09 <jbtschopp@gmail.com>
2020-04-06 19:34:41 -0300

    Implementación de SweetAlert2.js
    Librería de JavaScript que permite mostrar alertas

---
+++ assets/js/alertas.js
@@ -0,0 +1,5 @@
+Swal.fire({
+    title: 'MAPA INTERACTIVO COVID-19',
+    text: 'Te presentamos el nuevo mapa de soporte de datos y visualización para el proyecto de mapeo de OpenStreetMap relacionado a la actual pandemia del Covid-19. Puede visitarlo clic en el enlace \"COVID-19\" del menú.',
+    confirmButtonAlert: 'CONTINUAR A OSM ARGENTINA'
+})
