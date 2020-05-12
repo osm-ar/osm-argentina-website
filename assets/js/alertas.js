62ca32140f533263d95082efcfa4c8f872699302
Juan Blas Tschopp <jbtschopp@gmail.com>
2020-05-12 00:05:04 -0300

    #12 Actualización popup

--- assets/js/alertas.js
+++ assets/js/alertas.js
@@ -1,6 +1,8 @@
+/*
 Swal.fire({
     imageUrl: 'assets/img/coronaton.png',
     html:
         'El próximo <b>Sábado</b> y <b>Domingo</b> estaremos mapeando <u>centros de salud de las zonas más afectadas por el Coronavirus</u>, mas info haciendo <a href="https://wiki.openstreetmap.org/wiki/ES:Argentina/Eventos/Mapat%C3%B3n_Salud_2020">clic aquí</a>',
     button: "CONTINUAR A OSM ARGENTINA"
 })
+*/
